d0c6d772764fe93811a699cf5966eef862146f8d
Brian Weinreich <be.weinreich@gmail.com>
2015-12-04 11:12:19 -0500

    added bup to path

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 pushd .
-cp -r ./ ~/.bup
-cd ~/.bup
+cp -r ./ /usr/local/bup
+cd /usr/local/bup
 bundle install
 popd
-echo "Installed"
+
+for file in $(startup_files); do
+  [ -f $HOME/$file ] || continue
+  (grep "Added by Bup" $HOME/$file >/dev/null) && break
+
+  cat <<MESSAGE >>$HOME/$file
+
+### Added by Bup
+export PATH="/usr/local/bup/bin:\$PATH"
+MESSAGE
+
+  break
+done
+
+echo "You installed Bup!"
